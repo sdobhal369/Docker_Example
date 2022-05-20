54a3157cdcf8b515d7e857913ac6fec07996077b
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2022-05-20 14:32:04 +0530

    Update release-version.sh

--- release-version.sh
+++ release-version.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-sed -i'' -e '20s/[v]//' ios/LakeshoreProject/Info.plist
+sed -i'' -e '20s/[v]//' Info.plist
+
 latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+
     echo "The Latest git tag is" $latestTag
-    chmod a+x ios/LakeshoreProject/Info.plist
+    chmod a+x Info.plist
     
-version=$(sed -n '20s/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][0-9][^.]\).*/\1/p' ios/LakeshoreProject/Info.plist)
+version=$(sed -n '20s/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][0-9][^.]\).*/\1/p' Info.plist)
+
 a=v$version
 b=$latestTag 
+
 echo  "The version in Info.plist is" $a
 echo  "The Latest version is " $b
 
 if [ $a == $b ]
+
 then
 	#If they are equal then print this:::
 	echo "it is upto date"
 else
-    sed -i'' -e "${20}s#[0-9]\.[0-9]\.[0-9].*#"$version"</string>#" ios/LakeshoreProject/Info.plist
-    sed -i'' -e "${20}s/"$version"/"$latestTag"/" ios/LakeshoreProject/Info.plist
-    sed -i'' -e '20s/[v]//' ios/LakeshoreProject/Info.plist
-
+    sed -i'' -e "${20}s#[0-9]\.[0-9]\.[0-9].*#"$version"</string>#" Info.plist
+    sed -i'' -e "${20}s/"$version"/"$latestTag"/" Info.plist
+    sed -i'' -e '20s/[v]//' Info.plist
 	
 fi
