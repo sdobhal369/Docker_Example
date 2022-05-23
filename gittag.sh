dec01e99af0ca210ae987b55b5e20adbc0d4a231
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2022-05-23 11:16:20 +0530

    Update gittag.sh

--- gittag.sh
+++ gittag.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+#PlistBuddy command to read or write values in your info.plist file
+/usr/libexec/PlistBuddy -c 'print MARKETING_VERSION' Info.plist
+
 #get highest tag number
 #VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
 
-sed -n "20p" Info.plist > a.txt 
-VERSION=$(awk -v FS="(<string>|</string>)" '{print $2}' a.txt)
+#sed -n "20p" Info.plist > a.txt 
+#VERSION=$(awk -v FS="(<string>|</string>)" '{print $2}' a.txt)
 
 #replace . with space so can split into an array
-VERSION_BITS=(${VERSION//./ })
+#VERSION_BITS=(${VERSION//./ })
 
 #get number parts and increase last one by 1
-vNUM1=${VERSION_BITS[0]}
-vNUM2=${VERSION_BITS[1]}
-vNUM3=${VERSION_BITS[2]}
-vNUM3=$((vNUM3+1))
+#vNUM1=${VERSION_BITS[0]}
+#vNUM2=${VERSION_BITS[1]}
+#vNUM3=${VERSION_BITS[2]}
+#vNUM3=$((vNUM3+1))
 
 #create new tag
-NEW_TAG="$vNUM1.$vNUM2.$vNUM3"
+#NEW_TAG="$vNUM1.$vNUM2.$vNUM3"
 
-echo "Updating $VERSION to $NEW_TAG"
+#echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
 
-GIT_COMMIT=`git rev-parse HEAD`
+#GIT_COMMIT=`git rev-parse HEAD`
 
 #NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
@@ -30,9 +33,9 @@ GIT_COMMIT=`git rev-parse HEAD`
 
 #if [ -z "$NEEDS_TAG" ]; then
 
-    git tag -a $NEW_TAG -m $NEW_TAG
-    echo "Tagged with $NEW_TAG"
-    sed -i'' -e "${20}s/"$VERSION"/"$NEW_TAG"/" Info.plist
-    echo "New version Updated"
-    git add .
-    git commit -m "ECMA-5778 : Updated new BuildVersion in Info.plist"
+#    git tag -a $NEW_TAG -m $NEW_TAG
+#    echo "Tagged with $NEW_TAG"
+#    sed -i'' -e "${20}s/"$VERSION"/"$NEW_TAG"/" Info.plist
+#    echo "New version Updated"
+#    git add .
+#    git commit -m "ECMA-5778 : Updated new BuildVersion in Info.plist"
