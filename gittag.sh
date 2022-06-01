34d8e8499dd7fafa817c29f112b238080040f3da
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2022-06-01 15:21:30 +0530

    Create gittag.sh

---
+++ gittag.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function parse_git_dirty() {
+  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ]
+}
+
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+#Get current branch name
+GIT_BRANCH=$(parse_git_branch) 
+
+if [[ $GIT_BRANCH == "feature/E*/*" ]]
+then
+	echo "Hello I am if Condition"
+else
+  echo "Hello I am else Condition"	
+fi
